b7040f09cabda96dfe1303df1265f733af5ec96b
Taylor Osbourne <taylorgosbourne@gmail.com>
2020-06-18 16:39:25 -0400

    fun

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -6,51 +6,49 @@
  */
 
 module.exports = {
-    list: (req, res) => (
-        Articles.find({}).exec((err, articles) => {
-            if (err) {
-                res.send(500, { err })
-            }
-            res.view('pages/list', { articles: articles })
-        })
-    ),
-    add: (req, res) => res.view('pages/add'),
-    create: (req, res) => {
-        const title = req.body.title;
-        const body = req.body.body;
-        Articles.create({ title, body }).exec((err) => {
-            if (err) {
-                res.send(500, { err });
-            }
-            res.redirect('/articles/list');
-        })
-    },
-    delete: (req, res) => {
-        Articles.destroy({ id: req.params.id }).exec((err) => {
-            if (err) {
-                res.send(500, { err });
-            }
-            res.redirect('/articles/list');
-        })
-        return false;
-    },
-    edit: (req, res) => {
-        Articles.findOne({ id: req.params.id }).exec((err, article) => {
-            if (err) {
-                res.send(500, { err })
-            }
-            res.view('pages/edit', { article })
-        })
-    },
-    update: (req, res) => {
-        const title = req.body.title;
-        const body = req.body.body;
-        Articles.update({ id: req.params.id }, { title, body }).exec((err) => {
-            if (err) {
-                res.send(500, { err });
-            }
-            res.redirect('/articles/list');
-        })
-    }
+  list: (req, res) => (
+    Articles.find({}).exec((err, articles) => {
+      if (err) {
+        res.send(500, { err })
+      }
+      res.view('pages/list', { articles: articles })
+    })
+  ),
+  add: (req, res) => res.view('pages/add'),
+  create: (req, res) => {
+    const { title, body } = req.body;
+    Articles.create({ title, body }).exec((err) => {
+      if (err) {
+        res.send(500, { err });
+      }
+      res.redirect('/articles/list');
+    })
+  },
+  delete: (req, res) => {
+    Articles.destroy({ id: req.params.id }).exec((err) => {
+      if (err) {
+        res.send(500, { err });
+      }
+      res.redirect('/articles/list');
+    })
+    return false;
+  },
+  edit: (req, res) => {
+    Articles.findOne({ id: req.params.id }).exec((err, article) => {
+      if (err) {
+        res.send(500, { err })
+      }
+      res.view('pages/edit', { article })
+    })
+  },
+  update: (req, res) => {
+    const { title, body } = req.body;
+    Articles.update({ id: req.params.id }, { title, body }).exec((err) => {
+      if (err) {
+        res.send(500, { err });
+      }
+      res.redirect('/articles/list');
+    })
+  }
 };
 
